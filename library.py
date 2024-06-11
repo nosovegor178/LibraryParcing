8755e0a846beaca7c9d06bbb05651939d52f93ce
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-06-11 20:01:58 +0300

    again reassembled code and wrote README

--- library.py
+++ library.py
@@ -6,7 +6,10 @@ from urllib.parse import urljoin, urlparse
 import argparse
 
 
-def check_for_redirect(response):
+def check_for_redirect(book_number):
+    url = 'https://tululu.org/txt.php?id={}'.format(book_number)
+    response = requests.get(url)
+    response.raise_for_status()
     if response.history:
         raise requests.exceptions.HTTPError
     
@@ -14,7 +17,6 @@ def check_for_redirect(response):
 def download_file(url, filename, folder='Books'):
     response = requests.get(url)
     response.raise_for_status()
-    check_for_redirect(response)
     sanitized_filename = sanitize_filename(filename)
     path_to_file = os.path.join(folder, sanitized_filename) 
     if not os.path.exists(folder):
@@ -24,87 +26,67 @@ def download_file(url, filename, folder='Books'):
     return path_to_file
 
 
-def find_author_and_book_name(response):
+def parse_book_page(book_url, template_url = 'https://tululu.org'):
+    response = requests.get(book_url)
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
-    filename = soup.find('h1').text.split('::')
-    book_name = filename[0].strip()
-    author_name = filename[1].strip()
-    return book_name, author_name
-
-
-def get_book_image_url(response):
-    soup = BeautifulSoup(response.text, 'lxml')
+    book_name, book_author = soup.find('h1').text.split(' :: ')
     book_image_url = soup.find('div', class_='bookimage').find('img')['src']
-    parsed_url = urljoin('https://tululu.org', book_image_url)
-    return parsed_url
-
-
-def get_book_comments(response):
-    try:
-        soup = BeautifulSoup(response.text, 'lxml')
-        book_comments = soup.find_all('div', class_='texts')
-        founded_book_comments = []
-        for one_comment in book_comments:
-            founded_book_comments.append(one_comment.find('span').text)
-        return founded_book_comments
-    except AttributeError:
-        return []
-    
-
-def get_book_genres(response):
-    try:
-        soup = BeautifulSoup(response.text, 'lxml')
-        genres = soup.find('span', class_='d_book').find_all('a')
-        founded_genres = []
-        for one_genre in genres:
-            founded_genres.append(one_genre.text)
-        return founded_genres
-    except AttributeError:
-        return []
-
-
-def parse_book_page(content, book_number):
-    parsed_data = []
-    try:
-        url_for_checking = 'https://tululu.org/txt.php?id={}'.format(book_number)
-        response = requests.get(url_for_checking)
-        response.raise_for_status()
-        check_for_redirect(response)
-        
-        book_name = '{}. {}.txt'.format(book_number, find_author_and_book_name(content)[0])
-        comments = get_book_comments(content)
-        image_url = get_book_image_url(content)
-        image_name = urlparse(image_url).path.split('/')[-1]
-        genres = get_book_genres(content)
-        parsed_data.append({
-            'book_name': book_name,
-            'book_genres': genres,
-            'comments': comments,
-            'image_url': image_url,
-            'image_name': image_name
-        })
-    except requests.exceptions.HTTPError:
-        print('Книга отсутствует')
-    return parsed_data
+    image_url = urljoin(template_url, book_image_url)
+    image_name = urlparse(image_url).path.split('/')[-1]
+    book_comments = soup.find_all('div', class_='texts')
+    comments = [comment.find('span').text for comment in book_comments]
+    genres = soup.find('span', class_='d_book').find_all('a')
+    genres = [genre.text for genre in genres]
+    parsed_parametrs = {
+        'author': book_author.strip(),
+        'book_name': book_name.strip(),
+        'book_genres': genres,
+        'comments': comments,
+        'image_url': image_url,
+        'image_name': image_name
+    }
+    return parsed_parametrs
 
 
 def parse_all_books(start_id, end_id):
     parsing_results = []
     for book_number in range(start_id, end_id+1):
-        book_url = 'https://tululu.org/b{}'.format(book_number)
-        response = requests.get(book_url)
-        response.raise_for_status()
-        parsing_results.append(parse_book_page(response, book_number))
+        try:
+            check_for_redirect(book_number)
+            book_url = 'https://tululu.org/b{}'.format(book_number)
+            parsing_results.append(parse_book_page(book_url))
+        except requests.exceptions.HTTPError:
+            print('Книга отсутствует')
     return parsing_results
 
 
+def download_all_books_and_their_images(start_id, end_id):
+    for book_number in range(start_id, end_id+1):
+        try:
+            check_for_redirect(book_number)
+            url = 'https://tululu.org/txt.php?id={}'.format(book_number)
+            book_url = 'https://tululu.org/b{}'.format(book_number)
+            book_name = '{}. {}.txt'.format(book_number,
+                                        parse_book_page(book_url)['book_name'])
+            download_file(url, book_name)
+            download_file(parse_book_page(book_url)['image_url'],
+                            parse_book_page(book_url)['image_name'],
+                            'images')
+        except requests.exceptions.HTTPError:
+            print('Книга отсутствует')
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("start_id",
+parser.add_argument('start_id',
                     help='The number of book from which you are going to download',
                     type=int)
-parser.add_argument("end_id", 
+parser.add_argument('end_id', 
                     help='The number of book which will stop your downloading',
                     type=int)
 args = parser.parse_args()
 
-print(parse_all_books(args.start_id, args.end_id))
+
+if __name__ == '__main__':
+    download_all_books_and_their_images(args.start_id, args.end_id)
+    print(parse_all_books(args.start_id, args.end_id))
