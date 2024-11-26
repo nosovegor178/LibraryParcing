6dba8626337a6347045623244d00897457852e24
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-11-26 18:41:49 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -1,13 +1,14 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+import os
+import more_itertools
 
 
 def get_books():
     with open("./result/books.json", "r", encoding="utf-8") as my_file:
         file_content_json = my_file.read()
         books = json.loads(file_content_json)
-        my_file.close()
         return books
 
 
@@ -16,18 +17,23 @@ def rebuild():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    template = env.get_template('templates/template.html')
+    template = env.get_template('template.html')
     downloaded_books = get_books()
-    rendered_page = template.render(
-        books=downloaded_books
-    )
+    downloaded_books = list(more_itertools.chunked(downloaded_books, 20))
+    print(downloaded_books)
+    for page_number, page_json in enumerate(downloaded_books):
+        rendered_page = template.render(
+            books=page_json
+        )
 
-    with open('templates/index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+        os.makedirs('pages', exist_ok=True)
+
+        with open('pages/index{}.html'.format(page_number+1), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
     print('Server rebuilt')
 
 rebuild()
 
 server = Server()
-server.watch('templates/*.html', rebuild)
-server.serve(root='templates/index.html')
+server.watch('pages/*.html', rebuild)
+server.serve(root='pages/index1.html')
